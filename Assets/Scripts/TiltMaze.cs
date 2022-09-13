0254992aed1d8318c42d240cd80349cb10e05f14
Riukko <darkarceus10@hotmail.fr>
2022-09-13 14:36:24 +0200

    Tilt maze script (to make easier level design iterations)

---
+++ Assets/Scripts/TiltMaze.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltMaze : MonoBehaviour
+{
+    public float TiltXAmount;
+    public float TiltZAmount;
+
+    public float maxXRotation;
+    public float maxZRotation;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            TiltX(-TiltXAmount);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            TiltX(TiltXAmount);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            TiltZ(-TiltZAmount);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            TiltZ(TiltZAmount);
+        }
+    }
+
+    void TiltX(float amount)
+    {
+        Debug.Log(Mathf.Abs(Mathf.Repeat(transform.eulerAngles.x + 180, 360) - 180) + amount);
+        transform.Rotate(amount, 0, 0);
+        transform.rotation = Quaternion.Euler(Mathf.Clamp(Mathf.Repeat(transform.eulerAngles.x + 180, 360) - 180, -maxXRotation, maxXRotation), transform.eulerAngles.y, transform.eulerAngles.z);
+
+    }
+
+    void TiltZ(float amount)
+    {
+        transform.Rotate(0, 0, amount);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x , transform.eulerAngles.y, Mathf.Clamp(Mathf.Repeat(transform.eulerAngles.z + 180, 360) - 180, -maxZRotation, maxZRotation));
+    }
+
+}
